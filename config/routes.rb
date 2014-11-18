1a32cf5ed5b5a9c972102108ec7373ab7c9fd8d7
wojtusia@unlv.nevada.edu <wojtusia@unlv.nevada.edu>
2014-11-18 00:17:38 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :qr_codes, only: [:new, :create]
+  root to: "qr_codes#new"  
+  #get 'qr_codes/new'
+  
+  # get 'qr_codes/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
