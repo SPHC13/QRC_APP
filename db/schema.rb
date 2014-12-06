96762fc2e96ec7aef945fdd092a70b76d415081c
wojtusia@unlv.nevada.edu <wojtusia@unlv.nevada.edu>
2014-12-06 05:26:45 +0000

    Finally migrated my database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120002813) do
+ActiveRecord::Schema.define(version: 20141206052444) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141120002813) do
     t.string   "tag"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "event_id"
   end
 
 end
